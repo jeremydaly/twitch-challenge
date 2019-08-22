1a61c2a23406035e66a2cc16efd6f2a64809519c
Jeremy Daly <jeremy@jeremydaly.com>
2019-08-22 11:39:22 -0700

    update

--- handler.js
+++ handler.js
@@ -1,9 +1,18 @@
 'use strict';
 
-const AWS = require('aws-sdk');
+const AWS = require('aws-sdk')
+const data = new AWS.DynamoDB.DocumentClient()
 
 module.exports.graphql = async (event) => {
-  
+
+  const params = {
+    TableName: 'property-table'
+  }
+
+  let response = await data.scan(params).promise()
+
+  console.log(response);
+
   console.log('Received event {}', JSON.stringify(event, 3));
   console.log('Got an Invoke Request.');
 };
